f026e70e6f96ee6d32344266b9baf0a4c3a19532
quanghuy <quanghuy.h20@gmail.com>
2021-04-11 17:07:21 +0700

    Update some files

--- src/services/coursesAPI.js
+++ src/services/coursesAPI.js
@@ -4,6 +4,15 @@ const coursesAPI = {
   getCourses: () => {
     return axiosClient.get("/QuanLyKhoaHoc/LayDanhSachKhoaHoc");
   },
+  getCoursesByCategory: (category) => {
+    const params = {
+      maDanhMuc: category,
+    };
+    // return axiosClient.get(`/QuanLyKhoaHoc/LayKhoaHocTheoDanhMuc?maDanhMuc=${category}`);
+    return axiosClient.get("/QuanLyKhoaHoc/LayKhoaHocTheoDanhMuc", {
+      category,
+    });
+  },
 };
 
 export default coursesAPI;
